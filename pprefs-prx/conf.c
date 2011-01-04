e893c1c3ca003e24ca7178b2ef38cd41577d26d6
hiroizerone <hiroizerone@6accd9b7-f5dc-cdcc-7108-b63bce113ba2>
2011-01-04 10:31:29 +0000

    add New menu(Config) / add new option(onePushRestart) / re fix to display model

--- pprefs-prx/conf.c
+++ pprefs-prx/conf.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <stdio.h>
 #include "conf.h"
+#include "main.h"
 
 // プロトタイプ宣言
 int Check_EOF(SceUID fd);
@@ -117,7 +118,6 @@ bool Get_Bool(const char *str,bool defaultValue)
 {
 	bool rtn = defaultValue;
 	
-	// 最後まで
 	if( strcasecmp(str,"true") == 0 ){
 		rtn = true;
 	}else if(  strcasecmp(str,"false") == 0 ){
@@ -153,9 +153,8 @@ int Read_Conf(const char *path, Conf_Key *key)
 	*ptr	= '\0';
 
 	// 読み取れなっかった場合はデフォルトの設定
-	key->bootKey = PSP_CTRL_HOME;
-	key->swapButton = false;
-	key->bootMessage = true;
+	Set_Default_Conf(key);
+	
 	
 	// 設定ファイル・オープン
 	fd = sceIoOpen(ms_path, PSP_O_RDONLY, 0777);
@@ -188,7 +187,16 @@ int Read_Conf(const char *path, Conf_Key *key)
 			{
 				key->bootMessage = Get_Bool(ptr,true);
 			}
-			
+			else if(strcasecmp(buf, "ONEPUSHRESTART") == 0)
+			{
+				key->onePushRestart = Get_Bool(ptr,false);
+			}
+			/*
+			else if(strcasecmp(buf, "???") == 0)
+			{
+				key->??? = ???
+			}
+			*/
 		}
 	}
 
@@ -196,3 +204,204 @@ int Read_Conf(const char *path, Conf_Key *key)
 	return 0;
 }
 
+
+
+int Write_Conf(const char *path, Conf_Key *key)
+{
+	SceUID fd,fdw;
+	int readSize;
+	char buf[256],tmp[256];
+	char ms_path[128],ms_write_path[128];
+	char *ptr;
+	u32 flag = 0;
+	
+
+	// コピー
+	strcpy(ms_path, path);
+
+	// パス取得
+	ptr = strrchr(ms_path, '/');
+	ptr++;
+	*ptr++	= 'p';
+	*ptr++	= 'p';
+	*ptr++	= 'r';
+	*ptr++	= 'e';
+	*ptr++	= 'f';
+	*ptr++	= 's';
+	*ptr++	= '.';
+	*ptr++	= 'i';
+	*ptr++	= 'n';
+	*ptr++	= 'i';
+	*ptr	= '\0';
+
+	strcpy(ms_write_path, path);
+
+	ptr = strrchr(ms_write_path, '/');
+	ptr++;
+	*ptr++	= 'p';
+	*ptr++	= 'p';
+	*ptr++	= 'r';
+	*ptr++	= 'e';
+	*ptr++	= 'f';
+	*ptr++	= 's';
+	*ptr++	= '.';
+	*ptr++	= 'i';
+	*ptr++	= 'n';
+	*ptr++	= 'i';
+	*ptr++	= '.';
+	*ptr++	= 't';
+	*ptr++	= 'm';
+	*ptr++	= 'p';
+	*ptr	= '\0';
+
+	fdw = sceIoOpen(ms_write_path, PSP_O_WRONLY | PSP_O_CREAT | PSP_O_TRUNC, 0777);
+	if( fdw < 0 ) return -1;
+	
+	fd = sceIoOpen(ms_path, PSP_O_RDONLY, 0777);
+	
+
+	while( fd >= 0 ){
+		readSize = read_line_file_keepn(fd,buf,256);
+		if( readSize <= 0 ) break;
+		
+		
+		
+		strcpy(tmp,buf);
+		//余計なスペースを削除する
+		removeSpace(buf);
+		//コメントや改行だけの行など飛ばしてもいい行なら
+		if( buf[0] == '#' || buf[0] == '\n' || buf[0] == '\0' ){
+			sceIoWrite(fdw,tmp,readSize);
+			continue;
+		}
+
+		ptr = ch_token(buf, "=");
+		if(ptr == NULL) continue; //ダメな行
+		
+		if(strcasecmp(buf, "BOOTKEY") == 0 && (!(flag & 1)) )
+		{
+			tmp[0] = '\0';
+			GET_KEY_NAME_FOR_CONF(key->bootKey,tmp);
+			ptr = strrchr(tmp, '+');
+			if( ptr != NULL ) ptr[-1] = '\0';
+			sprintf(buf,"BootKey = %s\n",tmp);
+			flag |= 1;
+		}
+		else if(strcasecmp(buf, "SWAPBUTTON") == 0 && (!(flag & 2)) )
+		{
+			sprintf(buf,"SwapButton = %s\n",key->swapButton?"true":"false");
+			flag |= 2;
+		}
+		else if(strcasecmp(buf, "BOOTMESSAGE") == 0 && (!(flag & 4)) )
+		{
+			sprintf(buf,"BootMessage = %s\n",key->bootMessage?"true":"false");
+			flag |= 4;
+		}
+		else if(strcasecmp(buf, "ONEPUSHRESTART") == 0 && (!(flag & 8)) )
+		{
+			sprintf(buf,"OnePushRestart = %s\n",key->onePushRestart?"true":"false");
+			flag |= 8;
+		}
+		/*
+			else if(strcasecmp(buf, "???") == 0 && (!(flag & ?)) )
+			{
+				sprintf(buf,"??? = %s\n", ???);
+				flag |= ?;
+			}
+		*/
+		else
+		{
+			//ダメな行
+			continue;
+		}
+		sceIoWrite(fdw,buf,strlen(buf));
+	}
+	
+	
+	if( (!(flag & 1)) )
+	{
+		tmp[0] = '\0';
+		GET_KEY_NAME_FOR_CONF(key->bootKey,tmp);
+		ptr = strrchr(tmp, '+');
+		if( ptr != NULL ) ptr[-1] = '\0';
+		sprintf(buf,"BootKey = %s\n",tmp);
+		sceIoWrite(fdw,buf,strlen(buf));
+		flag |= 1;
+	}
+	if( (!(flag & 2)) )
+	{
+		sprintf(buf,"SwapButton = %s\n",key->swapButton?"true":"false");
+		sceIoWrite(fdw,buf,strlen(buf));
+		flag |= 2;
+	}
+	if( (!(flag & 4)) )
+	{
+		sprintf(buf,"BootMessage = %s\n",key->bootMessage?"true":"false");
+		sceIoWrite(fdw,buf,strlen(buf));
+		flag |= 4;
+	}
+	if( (!(flag & 4)) )
+	{
+		sprintf(buf,"BootMessage = %s\n",key->bootMessage?"true":"false");
+		sceIoWrite(fdw,buf,strlen(buf));
+		flag |= 4;
+	}
+	if( (!(flag & 8)) )
+	{
+		sprintf(buf,"OnePushRestart = %s\n",key->onePushRestart?"true":"false");
+		sceIoWrite(fdw,buf,strlen(buf));
+		flag |= 8;
+	}
+	/*
+	if( (!(flag & ?)) )
+	{
+		sprintf(buf,"??? = %s\n", ???);
+		sceIoWrite(fdw,buf,strlen(buf));
+		flag |= ?;
+	}
+	*/
+
+	if( fd >= 0 ) sceIoClose(fd);
+	sceIoClose(fdw);
+	
+	sceIoRemove(ms_path);
+	sceIoRename(ms_write_path,ms_path);
+	sceIoRemove(ms_write_path);
+	
+	return 0;
+}
+
+void Set_Default_Conf(Conf_Key *key)
+{
+	key->bootKey = PSP_CTRL_HOME;
+	key->swapButton = false;
+	key->bootMessage = true;
+	key->onePushRestart = false;
+	/*
+	key->??? = ???;
+	*/
+}
+
+//4箇所
+
+
+
+/*
+
+// @return:
+// true  とばしていい行
+// false とばしちゃダメ
+bool is_skiping_line(char *str)
+{
+	int i;
+	for( i = 0; str[i] != '\0'; i++ ){
+		if( str[i] == ' ' || str[i] == '\r' ){
+			continue;
+		}else if( str[i] == '#' || str[i] == '\n' || str[i] == '\0' ){
+			return true;
+		}
+	}
+	return false;
+}
+
+*/
