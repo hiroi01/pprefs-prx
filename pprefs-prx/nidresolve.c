530844e82e538d569d7663a099e1d42a99f57b40
hiroizerone <hiroizerone@6accd9b7-f5dc-cdcc-7108-b63bce113ba2>
2011-02-05 10:39:41 +0000

    re re fix nid resolve

--- pprefs-prx/nidresolve.c
+++ pprefs-prx/nidresolve.c
@@ -47,48 +47,15 @@ u32 Get_GetModelNid()
 		return 0x458A70B5;
 	}
 	
-	return 0;
+	return sceKernelGetModel;
 }
 
 void nidResolve(void)
 {
 	sceKernelDevkitVersion_Real = (void *)FindProc("sceSystemMemoryManager", "SysMemUserForUser", 0x3FC9AE6A);
 	
-	//resolve only 6.35PRO
-	if( sceKernelDevkitVersion_Real() == PSP_FIRMWARE(0x635) && sctrlHENGetVersion() == 0x1001  ){
-		sceKernelAllocPartitionMemory_Real	= (void *)FindProc("sceSystemMemoryManager", "SysMemUserForUser", 0x237DBD4F);
-		sceKernelGetBlockHeadAddr_Real		= (void *)FindProc("sceSystemMemoryManager", "SysMemUserForUser", 0x9D9A5BA1);
-		sceKernelFreePartitionMemory_Real	= (void *)FindProc("sceSystemMemoryManager", "SysMemUserForUser", 0xB6D61D02);
-//		sceKernelTotalFreeMemSize_Real		= (void *)FindProc("sceSystemMemoryManager", "SysMemUserForUser", 0xF919F628);
-		sceKernelGetModel_Real				= (void *)FindProc("sceSystemMemoryManager", "SysMemForKernel", Get_GetModelNid());
-		sceKernelLibcTime_Real				= (void *)FindProc("sceSystemMemoryManager", "UtilsForUser", 0x27CC57F0);
 
-		sceCtrlPeekBufferPositive_Real	= (void *)FindProc("sceController_Service", "sceCtrl_driver", 0x18654FC0);
-		sceCtrlSetSamplingCycle_Real	= (void *)FindProc("sceController_Service", "sceCtrl_driver", 0x855C255D);
-		sceCtrlSetSamplingMode_Real		= (void *)FindProc("sceController_Service", "sceCtrl_driver", 0x6CB49301);
 
-		sceDisplayGetFrameBuf_Real		= (void *)FindProc("sceDisplay_Service", "sceDisplay_driver", 0x08A10838);
-		sceDisplaySetFrameBuf_Real		= (void *)FindProc("sceDisplay_Service", "sceDisplay_driver", 0x37533141);
-		sceDisplayWaitVblankStart_Real	= (void *)FindProc("sceDisplay_Service", "sceDisplay_driver", 0xC30D327D);
-	}else{
-
-		sceKernelAllocPartitionMemory_Real	= sceKernelAllocPartitionMemory;
-		sceKernelGetBlockHeadAddr_Real		= sceKernelGetBlockHeadAddr;
-		sceKernelFreePartitionMemory_Real	= sceKernelFreePartitionMemory;
-//		sceKernelTotalFreeMemSize_Real		= sceKernelTotalFreeMemSize;
-		sceKernelGetModel_Real				= sceKernelGetModel;
-		sceKernelLibcTime_Real				= sceKernelLibcTime;
-
-		sceCtrlPeekBufferPositive_Real	= sceCtrlPeekBufferPositive;
-		sceCtrlSetSamplingCycle_Real	= sceCtrlSetSamplingCycle;
-		sceCtrlSetSamplingMode_Real		= sceCtrlSetSamplingMode;
-
-		sceDisplayGetFrameBuf_Real		= sceDisplayGetFrameBuf;
-		sceDisplaySetFrameBuf_Real		= sceDisplaySetFrameBuf;
-		sceDisplayWaitVblankStart_Real	= sceDisplayWaitVblankStart;
-	}
-
-/*
 	sceKernelAllocPartitionMemory_Real	= (void *)FindProc("sceSystemMemoryManager", "SysMemUserForUser", 0x237DBD4F);
 	sceKernelGetBlockHeadAddr_Real		= (void *)FindProc("sceSystemMemoryManager", "SysMemUserForUser", 0x9D9A5BA1);
 	sceKernelFreePartitionMemory_Real	= (void *)FindProc("sceSystemMemoryManager", "SysMemUserForUser", 0xB6D61D02);
@@ -97,8 +64,8 @@ void nidResolve(void)
 	sceKernelGetModel_Real				= (void *)FindProc("sceSystemMemoryManager", "SysMemForKernel", Get_GetModelNid());
 	sceKernelLibcTime_Real				= (void *)FindProc("sceSystemMemoryManager", "UtilsForUser", 0x27CC57F0);
 	
-	int ver = sceKernelDevkitVersion_Real();
-	if( ver >= PSP_FIRMWARE(0x635) ){
+
+	if( sceKernelDevkitVersion_Real() >= PSP_FIRMWARE(0x635) ){
 		sceCtrlPeekBufferPositive_Real	= (void *)FindProc("sceController_Service", "sceCtrl_driver", 0x18654FC0);
 		sceCtrlSetSamplingCycle_Real	= (void *)FindProc("sceController_Service", "sceCtrl_driver", 0x855C255D);
 		sceCtrlSetSamplingMode_Real		= (void *)FindProc("sceController_Service", "sceCtrl_driver", 0x6CB49301);
@@ -125,6 +92,6 @@ void nidResolve(void)
 //		sceDisplayWaitVblankStart_Real	= (void *)FindProc("sceDisplay_Service", "sceDisplay", 0x984C27E7);
 		
 	}
-*/
+
 
 }
